be40906810d1680a2c22d7b21c4a363fda13fcd4
Usman <mac@MacBook-Air.local>
2026-05-06 17:58:47 +0100

    fix sequelize config syntax for production

--- iatrics_backend/config/config.js
+++ iatrics_backend/config/config.js
@@ -2,19 +2,31 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    use_env_variable: "DATABASE_URL",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
   },
+
   test: {
-    use_env_variable: "DATABASE_URL",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
   },
+
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
-development: {
-  use_env_variable: "DATABASE_URL",
-  dialect: "postgres",
-},
